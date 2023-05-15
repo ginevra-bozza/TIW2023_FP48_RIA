d0efd5deb5bb340015c861bb294ae190c900622b
emacimino <100288601+emacimino@users.noreply.github.com>
2023-05-15 10:57:29 +0200

    Revert "update loginManagement"
    This reverts commit 0b89f8e416dc6a79f9fe6a5ea4371ff95661377b.

--- src/main/webapp/JS/loginManagement.js
+++ src/main/webapp/JS/loginManagement.js
@@ -16,7 +16,7 @@
 						console.log("Status: "+x.status);
 						switch (x.status) {
 							case 200:
-								sessionStorage.setItem('namename', message);
+								sessionStorage.setItem('email', message);
 								window.location.href = "Home.html";
 								break;
 							case 400: // bad request
